cf2f9d732e47ef9e2f6498bb86ed0f32acd1b367
Sergio <128714385+SmezaG@users.noreply.github.com>
2024-02-13 23:50:00 +0100

    Initial Commit
    Initial Commit

---
+++ server/app.py
@@ -0,0 +1,33 @@
+# /SerangusChat/server/app.py
+
+from flask import Flask, render_template, request, session
+from flask_socketio import SocketIO, emit
+import os
+
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'templates'))
+app.config['SECRET_KEY'] = 'your_secret_key'  # Cambia esto por una clave secreta segura
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    # Solicitar nombre de usuario al conectarse
+    emit('request_username')
+
+@socketio.on('username_response')
+def handle_username_response(data):
+    # Asignar nombre de usuario a la sesión y notificar a los demás usuarios
+    session['username'] = data['username']
+    emit('user_connected', {'username': session['username']}, broadcast=True)
+
+@socketio.on('message')
+def handle_message(msg):
+    # Enviar mensaje con nombre de usuario
+    emit('message', {'username': session['username'], 'message': msg['message']}, broadcast=True)
+    # emit("message",{'username': "abc", 'message': "cde"},broadcast=False)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
